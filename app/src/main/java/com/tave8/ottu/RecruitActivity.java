f0e653aa0cfebbb4bd424d66f1c04915c2f0993a
youngniw <1999julie@naver.com>
2021-11-30 15:02:52 +0900

    recruit, recruiting UI

---
+++ app/src/main/java/com/tave8/ottu/RecruitActivity.java
@@ -0,0 +1,13 @@
+package com.tave8.ottu;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RecruitActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recruit);
+    }
+}
